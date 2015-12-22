b5cebb80365f1903785ba71ee6ef8398ce9e0a42
Sergey <sergey.krasilovets@gmail.com>
2015-12-22 22:04:48 +0200

    +stack

---
+++ src/com/company/Stack.java
@@ -0,0 +1,34 @@
+package com.company;
+
+class Stack {
+    int stck[] = new int[10];
+    int tos;
+
+    Stack() {tos = -1;}
+
+    void push(int item) {
+        if (tos == 9) System.out.println("Stack is full");
+        else stck[++tos] = item;}
+
+    int pop() {
+        if (tos < 0) {System.out.println("Stack is not full");return 0;}
+        else return stck[tos--];}
+}
+
+class TestStack {
+    public static void main(String[] args) {
+        Stack mystack1 = new Stack();
+        Stack mystack2 = new Stack();
+
+        for (int i=0; i<10; i++) mystack1.push(i);
+        for (int i=10; i<20; i++) mystack2.push(i);
+
+        System.out.println("Stack 1 contains:");
+        for (int i=0; i<10; i++)
+        System.out.print(mystack1.pop());
+
+        System.out.println("Stack 2 contains:");
+        for (int i=0; i<10; i++)
+        System.out.println(mystack2.pop());
+    }
+}
