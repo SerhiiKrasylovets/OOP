f910595ea1b562d2f47cd80c550e58672e93a33b
Sergey <sergey.krasilovets@gmail.com>
2016-01-15 22:35:43 +0200

    +hometask

--- src/strings/HomeTask.java
+++ src/strings/HomeTask.java
@@ -1,6 +1,21 @@
 package strings;
 
-public class HomeTask {
-    //массив строк вывести в алфавитном порядке, int compare to
-    //метод intern
+//массив строк вывести в алфавитном порядке, int compare to
+//метод intern
+
+class HomeTask {
+    static String alphabet[] = {"aaa", "aba", "abc", "aab", "bac"};
+
+    public static void main(String[] args) {
+        for (int i = 0; i < alphabet.length; i++) {
+            for (int j = i + 1; j < alphabet.length; j++) {
+                if (alphabet[j].compareTo(alphabet[i]) < 0) {
+                    String k = alphabet[i];
+                    alphabet[i] = alphabet[j];
+                    alphabet[j] = k;
+                }
+            }
+            System.out.println(alphabet[i]);
+        }
+    }
 }
