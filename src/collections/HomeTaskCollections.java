24af78bc551ee98c158f98ea21ea30d8b3688949
Nelipa Dmitriy <nelipa.dmitriy@mail.ru>
2016-01-24 14:28:01 +0200

    24.01

---
+++ src/collections/HomeTaskCollections.java
@@ -0,0 +1,7 @@
+package collections;
+
+public class HomeTaskCollections {
+    /*провести исследовани - ArrayList и LinkedList добавить/удалить в начало, средину, конец, засечь время,
+    добавить множество элементов (от 500)
+    засечь время методом System.nanoTime()*/
+}
