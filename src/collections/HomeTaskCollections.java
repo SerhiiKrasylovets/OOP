c33cf1be650188c544eefd00da0f5b97d2188d5e
SKrasilovets <sergey.krasilovets@gmail.com>
2016-01-29 17:34:38 +0200

    home task

--- src/collections/HomeTaskCollections.java
+++ src/collections/HomeTaskCollections.java
@@ -1,4 +1,4 @@
-/*провести исследовани - ArrayList и LinkedList добавить/удалить в начало, средину, конец, засечь время,
+/*провести исследование - ArrayList и LinkedList добавить/удалить в начало, средину, конец, засечь время,
     добавить множество элементов (от 500)
     засечь время методом System.nanoTime()*/
 
@@ -9,7 +9,7 @@ public class HomeTaskCollections {
 
     public static void main(String[] args) {
         Random rand = new Random();
-        int value = 1000000, iter = 500;
+        int value = 1000000, iter = 100;
 
         ArrayList<Object> al = new ArrayList<>();
         LinkedList<Object> ll = new LinkedList<>();
@@ -17,74 +17,79 @@ public class HomeTaskCollections {
         {long timeal = System.nanoTime();
             for (int i = 0; i < iter; i++){al.add(rand.nextInt(value));}
             long diffal = System.nanoTime() - timeal;
-            //System.out.println("ArrayList add elements time = " + diffal);
         long timell = System.nanoTime();
             for (int i = 0; i < iter; i++){ll.add(rand.nextInt(value));}
             long diffll = System.nanoTime() - timell;
-            //System.out.println("LinkedList add elements time = " + diffll);
-        if (diffal < diffll ) System.out.println("ArrayList was quicker than LinkedList in " + (diffll-diffal) + " ns");
-                else System.out.println("LinkedList was quicker than ArrayList in " + (diffal-diffll) + " ns");
-            System.out.println();}
+        if (diffal < diffll )
+            System.out.println("Adding values: ArrayList was quicker than LinkedList by " + (diffll-diffal) + " ns");
+                else
+            System.out.println("Adding values: LinkedList was quicker than ArrayList by " + (diffal-diffll) + " ns");}
 
-        {long time = System.nanoTime();
+        {long timeal = System.nanoTime();
             for (int i = 0; i < iter; i++){al.add(0, rand.nextInt(value));}
-            long diff = System.nanoTime() - time;
-            System.out.println("ArrayList add elements in the beginning time = " + diff);}
-        {long time = System.nanoTime();
+            long diffal = System.nanoTime() - timeal;
+        long timell = System.nanoTime();
             for (int i = 0; i < iter; i++){ll.addFirst(rand.nextInt(value));}
-            long diff = System.nanoTime() - time;
-            System.out.println("LinkedList add elements in the beginning time = " + diff);
-            System.out.println();}
+            long diffll = System.nanoTime() - timell;
+        if (diffal < diffll )
+                System.out.println("Adding values in the beginning: ArrayList was quicker than LinkedList by " + (diffll-diffal) + " ns");
+            else
+                System.out.println("Adding values in the beginning: LinkedList was quicker than ArrayList by " + (diffal-diffll) + " ns");}
 
-        {long time = System.nanoTime();
+        {long timeal = System.nanoTime();
             for (int i = 0; i < iter; i++){al.add(al.size(), rand.nextInt(value));}
-            long diff = System.nanoTime() - time;
-            System.out.println("ArrayList add elements int the end time = " + diff);}
-        {long time = System.nanoTime();
+            long diffal = System.nanoTime() - timeal;
+        long timell = System.nanoTime();
             for (int i = 0; i < iter; i++){ll.addLast(rand.nextInt(value));}
-            long diff = System.nanoTime() - time;
-            System.out.println("LinkedList add elements in the end time = " + diff);
-            System.out.println();}
+            long diffll = System.nanoTime() - timell;
+        if (diffal < diffll )
+                System.out.println("Adding values in the end: ArrayList was quicker than LinkedList by " + (diffll-diffal) + " ns");
+            else
+                System.out.println("Adding values in the end: LinkedList was quicker than ArrayList by " + (diffal-diffll) + " ns");}
 
-        {long time = System.nanoTime();
+        {long timeal = System.nanoTime();
             for (int i = 0; i < iter; i++){al.add(al.size()/2, rand.nextInt(value));}
-            long diff = System.nanoTime() - time;
-            System.out.println("ArrayList add elements int the middle time = " + diff);}
-        {long time = System.nanoTime();
+            long diffal = System.nanoTime() - timeal;
+        long timell = System.nanoTime();
             for (int i = 0; i < iter; i++){ll.add(ll.size()/2, rand.nextInt(value));}
-            long diff = System.nanoTime() - time;
-            System.out.println("LinkedList add elements in the middle time = " + diff);
-            System.out.println();}
+            long diffll = System.nanoTime() - timell;
+        if (diffal < diffll )
+                System.out.println("Adding values in the middle: ArrayList was quicker than LinkedList by " + (diffll-diffal) + " ns");
+            else
+                System.out.println("Adding values in the middle: LinkedList was quicker than ArrayList by " + (diffal-diffll) + " ns");}
 
-        {long time = System.nanoTime();
+        {long timeal = System.nanoTime();
             for (int i = 0; i < iter; i++){al.remove(0);}
-            long diff = System.nanoTime() - time;
-            System.out.println("ArrayList remove elements from the beginning time = " + diff);}
-        {long time = System.nanoTime();
+            long diffal = System.nanoTime() - timeal;
+        long timell = System.nanoTime();
             for (int i = 0; i < iter; i++){ll.removeFirst();}
-            long diff = System.nanoTime() - time;
-            System.out.println("LinkedList remove elements from the beginning time = " + diff);
-            System.out.println();}
+            long diffll = System.nanoTime() - timell;
+        if (diffal < diffll )
+                System.out.println("Removing values from the beginning: ArrayList was quicker than LinkedList by " + (diffll-diffal) + " ns");
+            else
+                System.out.println("Removing values from the beginning: LinkedList was quicker than ArrayList by " + (diffal-diffll) + " ns");}
 
-        {long time = System.nanoTime();
+        {long timeal = System.nanoTime();
             for (int i = 0; i < iter; i++){al.remove(al.size()-1);}
-            long diff = System.nanoTime() - time;
-            System.out.println("ArrayList remove elements from the end time = " + diff);}
-        {long time = System.nanoTime();
+            long diffal = System.nanoTime() - timeal;
+        long timell = System.nanoTime();
             for (int i = 0; i < iter; i++){ll.removeLast();}
-            long diff = System.nanoTime() - time;
-            System.out.println("LinkedList remove elements from the end time = " + diff);
-            System.out.println();}
+            long diffll = System.nanoTime() - timell;
+        if (diffal < diffll )
+                System.out.println("Removing values from the end: ArrayList was quicker than LinkedList by " + (diffll-diffal) + " ns");
+            else
+                System.out.println("Removing values from the end: LinkedList was quicker than ArrayList by " + (diffal-diffll) + " ns");}
 
-        {long time = System.nanoTime();
+        {long timeal = System.nanoTime();
             for (int i = 0; i < iter; i++){al.remove(al.size()/2);}
-            long diff = System.nanoTime() - time;
-            System.out.println("ArrayList remove elements from the middle time = " + diff);}
-        {long time = System.nanoTime();
+            long diffal = System.nanoTime() - timeal;
+        long timell = System.nanoTime();
             for (int i = 0; i < iter; i++){ll.remove(al.size()/2);}
-            long diff = System.nanoTime() - time;
-            System.out.println("ArrayList remove elements from the middle time = " + diff);
-            System.out.println();}
+            long diffll = System.nanoTime() - timell;
+        if (diffal < diffll )
+                System.out.println("Removing values from the middle: ArrayList was quicker than LinkedList by " + (diffll-diffal) + " ns");
+            else
+                System.out.println("Removing values from the middle: LinkedList was quicker than ArrayList by " + (diffal-diffll) + " ns");}
 
         }
     }
